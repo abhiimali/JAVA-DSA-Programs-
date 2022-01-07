e23fc1bc06ad0cb6189ffe56a0604750bfa5033b
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-01-07 23:37:29 +0530

    Time: 150 ms (5.06%), Space: 39.4 MB (46.16%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,7 +1,33 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        int n=nums.length-k;
-        return nums[n];
+    public int findKthLargest(int[] arr, int k) {
+        boolean swapped = false;
+        for (int i = 0; i < arr.length; i++) {  //  Outer Loop Is For Passed
+           // swapped = false;
+            // For Each Index , Max Item Will  Come At Last Respective Index
+
+            for (int j = 1; j < arr.length - i ; j++) {
+                // Swap if J Is Small Than J-1
+
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true;
+                }
+
+
+            }
+            if (swapped == false) {
+                break;
+
+            }
+
+        }
+//        int m = 
+//        if (arr[arr.length - k] != arr.length ])
+//        {
+//
+//        }
+        return arr[arr.length-k];
     }
 }
