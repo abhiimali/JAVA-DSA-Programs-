c6c52a7aaa10fa0a56110be44280915d5a2e06a3
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-01-27 18:35:13 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (49.17%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,27 +1,38 @@
 class Solution {
     public int search(int[] arr, int target) {
-        int start = 0 ;
-        int end = arr.length-1;
-        while (start<=end)
+        
+        int ans = searchB(arr,target,0 , arr.length -1);
+        return ans ; 
+    }
+    
+    int searchB(int[] arr, int target , int start , int end ) {
+
+
+        // BASE CONDITION
+        if (start>end)
         {
-            int mid = start + (end-start)/2;
+            return  -1 ;
+        }
 
-            if(target < arr[mid] )
-            {
-                end = mid - 1 ;
+        int mid = start + (end-start) / 2 ;
 
-            }
-            if (target > arr[mid])
-            {
-                start = mid + 1 ;
-            }
-            if (target == arr[mid])
-            {
-                return  mid ;
-            }
+        if (arr[mid] == target)
+        {
+            return  mid ;
 
         }
-         //System.out.println(" Nothing Found ");
-        return -1 ;
+        // if target is in left part Of Array Then Do Following
+
+        if (target < arr[mid])
+        {
+            return  searchB(arr, target, start, mid-1);
+        }
+
+        // if target is In Right Part Of array  Then Do Following
+
+        return  searchB(arr, target, mid+1, end);
+
+
     }
+    
 }
