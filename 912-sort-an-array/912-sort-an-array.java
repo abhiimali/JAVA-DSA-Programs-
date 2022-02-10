7d704f81e8f57342050fde2595ffd25ecf8dbf00
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-02-10 20:58:56 +0530

    Time: 9 ms (88.15%), Space: 66.2 MB (37.50%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,34 +1,49 @@
 class Solution {
-public int[] sortArray(int[] nums) {
-sort(nums , 0 , nums.length -1);
-// System.out.println(Arrays.toString(nums));
-return nums;
+    public int[] sortArray(int[] nums) {
+        
+      //  Arrays.sort(nums); 
+        
+        sort(nums,0,nums.length-1);
+        return nums ;
 
-}
-public void sort(int[] nums , int low , int hi){
-if (low>=hi){
-return;
-}
-int start = low;
-int end = hi;
-int mid = start + (end - start)/2;
-int pivot = nums[mid];
-while (start<=end){
-while (nums[start]<pivot){
-start++;
-}
-while (nums[end]>pivot){
-end--;
-}
-if (start<=end){
-int temp = nums[start];
-nums[start] = nums[end];
-nums[end] = temp;
-start++;
-end--;
-}
-}
-sort(nums , low , end);
-sort(nums , start , hi);
-}
+    }
+    
+    void  sort(int[] nums , int low , int high )
+    {
+        if (low >= high) {
+            return;
+        }
+
+        int start = low ;
+        int end  = high ;
+        int mid = start+(end-start) / 2 ;
+        int pivot = nums[mid] ;
+        while (start<=end)
+        {
+            // CODE FOR VOILETION
+
+            while (nums[start] < pivot)
+            {
+                start ++ ;
+            }
+            while (nums[end]>pivot)
+            {
+                end -- ;
+
+            }
+            if (start<=end)
+            {
+                 int temp = nums[start];
+                 nums[start]=nums[end];
+                 nums[end]=temp;
+                 start++;
+                 end--;
+
+            }
+        }
+
+            sort(nums,low,end);
+            sort(nums,start,high);
+
+    }
 }
