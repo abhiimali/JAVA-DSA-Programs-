9621bc5aa2365ecbda476783c4fdf3057b278b3c
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-02-28 12:28:14 +0530

    Time: 1 ms (71.52%), Space: 43.1 MB (25.15%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -9,42 +9,36 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-         if(list1 == null)
-            return list2;
-        if(list2 == null)
-            return list1;
-        ListNode head = null;
-        ListNode node = new ListNode();
-        while(list1!= null && list2!=null){        
-            if(list1.val > list2.val){
-                node.next = list2; 
-                list2=list2.next;
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy=new ListNode();
+        //Initially dummy is empty or you can say it is null
+        //we need another pointer reference for dummy variable
+        ListNode dd=dummy;
+        
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val)
+            {
+                dd.next=new ListNode(l1.val);
+                l1=l1.next;
+            }else{
+                dd.next=new ListNode(l2.val);
+                l2=l2.next;
             }
-            else{
-                node.next = list1 ; 
-                list1=list1.next;
-            }
-            if(head==null)
-                head = node.next;
-                
-            node = node.next; 
+            dd=dd.next;
         }
         
-        while(list1 != null)
-        {
-            node.next = list1;
-            list1 = list1.next;
-            node = node.next;
+        while(l1!=null){
+            dd.next=new ListNode(l1.val);
+            l1=l1.next;
+            dd=dd.next;
         }
+        while(l2!=null){
+            dd.next=new ListNode(l2.val);
+            l2=l2.next;
+            dd=dd.next;
+        }
+        return dummy.next;
         
-        while(list2 != null)
-        {
-            node.next = list2; 
-            list2 = list2.next;
-            node = node.next;
-        }
-        return head;
         
     }
 }
