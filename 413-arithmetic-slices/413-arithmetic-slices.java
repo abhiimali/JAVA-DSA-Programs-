2347380023bc8953a6b19cc033e6e806105e24b9
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-03-03 21:04:23 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (48.80%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -23,7 +23,44 @@ class Solution {
 }
 
 
-/*  Explanation :)  */
+/*  Explanation :)  
+
+
+pepcoding 
+
+      
+      create new array  empty one 
+      
+      if subarray found add it to the array position 
+      
+     these lines are basically for to check subarray at current postion
+     
+     1  3  5  7 
+     
+     if i is at 7 
+     then check 7-5 ==  5-3  as showns follow
+     
+     if(nums[i]-nums[i-1] == nums[i-1]-nums[i-2])
+            {
+                dp[i] = dp[i-1] + 1 ;
+                
+                ans += dp[i];
+                
+            }
+      
+      if it is then add it to array with including prev array i-1 count 
+      
+      update ans 
+      
+      
+      
+      
+     
+     
+     */
+
+
+
 
 
 
