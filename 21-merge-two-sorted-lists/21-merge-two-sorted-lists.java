b5fd65c05eac2c2402c687467e8a276cf7fd278f
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-03-04 13:03:40 +0530

    Time: 1 ms (72.01%), Space: 44.1 MB (7.21%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -10,35 +10,27 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy=new ListNode();
-        //Initially dummy is empty or you can say it is null
-        //we need another pointer reference for dummy variable
-        ListNode dd=dummy;
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        
+        ListNode temp=new ListNode(0);
+        ListNode dummy=temp;
         
         while(l1!=null && l2!=null){
-            if(l1.val<l2.val)
-            {
-                dd.next=new ListNode(l1.val);
+            if(l1.val<l2.val){
+                dummy.next=l1;
                 l1=l1.next;
-            }else{
-                dd.next=new ListNode(l2.val);
+            }
+            else{
+                dummy.next=l2;
                 l2=l2.next;
             }
-            dd=dd.next;
+            dummy=dummy.next;
         }
         
-        while(l1!=null){
-            dd.next=new ListNode(l1.val);
-            l1=l1.next;
-            dd=dd.next;
-        }
-        while(l2!=null){
-            dd.next=new ListNode(l2.val);
-            l2=l2.next;
-            dd=dd.next;
-        }
-        return dummy.next;
+        if(l1!=null) dummy.next=l1;
+        if(l2!=null) dummy.next=l2;
         
-        
+        return temp.next;
     }
 }
