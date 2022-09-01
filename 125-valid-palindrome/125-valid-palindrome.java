26f44902ee53e54183a615fcdc8a859f659a6f0e
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-09-01 09:35:29 +0530

    Time: 1457 ms (5.01%), Space: 47.4 MB (26.55%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,13 +1,36 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        
+        
+//          Logic 
+        
+//         1. Use Regular Expression For Converting removing alphanumeric values 
+            
+//             And then do lower Case ;
+            
+//             [^a-zA-Z0-9]
+             
+//         2. Now Loop Through String 
+            
+       
         s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        int left =0;
-        int right = s.length()-1;
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) return false;
+        
+            int  left=0;
+            int right=s.length()-1;
+        
+        while( left < right) 
+        {
+             if(s.charAt(left) != s.charAt(right))
+             {
+                 return false ;
+             }
+            
             left++;
             right--;
+            
         }
-        return true;
+
+        return true ;
+        
     }
 }
