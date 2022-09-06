480dbe50dbe2064bf12166ff3fa79c3fe71a8ba4
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-09-06 10:00:03 +0530

    Time: 10 ms (8.03%), Space: 43.1 MB (40.47%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.java
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -3,13 +3,13 @@ class Solution {
         int n = code.length;
         int[] res = new int[n];
         if (k == 0) return res;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             if (k > 0) {
-                for (int j = i + 1; j <= i + k; ++j) {
+                for (int j = i + 1; j <= i + k; j++) {
                     res[i] += code[j % n];
                 }
             } else {
-                for (int j = i + k; j <= i - 1; ++j) {
+                for (int j = i + k; j <= i - 1; j++) {
                     res[i] += code[(j + n ) % n];
                 }
             }
