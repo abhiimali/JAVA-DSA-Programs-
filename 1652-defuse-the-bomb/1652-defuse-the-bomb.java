46f3de512e43fd2395ec64a699a1479f8e5a7fbb
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-09-06 10:14:46 +0530

    Time: 3 ms (47.83%), Space: 42.8 MB (55.18%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.java
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -1,19 +1,39 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
-        int n = code.length;
-        int[] res = new int[n];
-        if (k == 0) return res;
-        for (int i = 0; i < n; i++) {
-            if (k > 0) {
-                for (int j = i + 1; j <= i + k; j++) {
+     
+        int[] res = new int[code.length];
+        int n = code.length ;
+        if(k == 0 ) return res ;
+        for(int i=0 ; i < code.length ; i++)
+        {
+            if(k>0)
+            {
+                for(int j=i+1;j<=i+k;j++)
+                {
                     res[i] += code[j % n];
                 }
-            } else {
-                for (int j = i + k; j <= i - 1; j++) {
-                    res[i] += code[(j + n ) % n];
-                }
+                
+                
             }
+            
+            else
+            {
+                for(int j=i+k ; j <= i - 1 ; j++ )
+                {
+                    res[i] += code[(j+n) % n] ;
+                    
+                }
+                
+                
+            }
+            
+            
+            
+            
+            
         }
-        return res;
+         
+        return res ;
+        
     }
 }
