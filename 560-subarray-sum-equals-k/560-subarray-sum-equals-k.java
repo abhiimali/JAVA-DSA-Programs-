82d6eb98d8c8812ca438431ca82769fb397735f5
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-09-07 11:05:59 +0530

    Time: 2051 ms (8.64%), Space: 54.8 MB (79.33%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,23 +1,43 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-             int ans=0;
-              for(int j=i;j<nums.length;j++){
-                  ans+=nums[j];
-                  if(ans==k){
-                      
-                           count++;
-                 
-                    }
-               } 
-           }
-            return count;
+        int count = 0;
+        
+        int n = nums.length ;
+        
+        int[] prefixSumArr = new int[n+1];
+        prefixSumArr[0]=0;
+        for(int index=1; index <= n ; index++)
+        {
+            
+             prefixSumArr[index]= prefixSumArr[index-1] + nums[index-1];
+             
+            
+         }
+        
+        
+        for(int i=0; i <= n  ; i++)
+        {
+            
+            for(int j = i+1 ; j<= n ; j++)
+            {
+                if(prefixSumArr[j]-prefixSumArr[i]==k)
+                {
+                    count++;
+                }
+                
+                
+                
+            }
+        }
+        
+        
+        return count;
+        
+        
+        
+        
+        
+        
+        
     }
 }
-
-//https://leetcode.com/problems/subarray-sum-equals-k/discuss/1532102/JAVA-oror-Picture-%2B-Detail-Explanation-oror-2-methods-oror-prefix-sum-%2B-HashMap-oror-Easy-Solution
-
-
-
-
