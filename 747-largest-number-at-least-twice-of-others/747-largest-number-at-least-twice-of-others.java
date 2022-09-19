9b75158cf3e9d2e4749b3d1e5f861213e4c53d08
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-09-19 22:20:52 +0530

    Time: 1 ms (85.78%), Space: 41.9 MB (64.59%) - LeetHub

--- 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.java
+++ 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.java
@@ -1,33 +1,23 @@
 class Solution {
     public int dominantIndex(int[] nums) {
      
-        HashMap<Integer,Integer> map = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+        int index = -1;
         
-        for(int i=0 ;i < nums.length ; i++)
-        {
-            map.put(nums[i],i);
+        for(int i = 0; i < nums.length; ++i){
+            if(max < nums[i]) {
+                max = nums[i];
+                index = i;
+            }
         }
-        
-        Arrays.sort(nums);
-        int n=nums.length ;
-        
-        for(int i=n-1 ; i>0 ; i-- )
-        {
-          //  System.out.println(nums[i]);
-            if(nums[i] >= nums[i-1] * 2)
-            {
-                return map.get(nums[i]);
-            }
-            else
-            {
-                return -1;
-            }
             
+        for(int i = 0; i < nums.length; ++i){
+            if(i == index) continue;
+            
+            if(nums[i] * 2 > max) return -1;
         }
         
-        return -1 ;
-    
-        
-        
+        return index;
+
     }
 }
