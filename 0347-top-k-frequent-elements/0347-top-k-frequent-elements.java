1b55973cdb458e6d967b06c29a8be7305ae0cc6a
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-10-20 19:50:56 +0530

    Time: 36 ms (25.28%), Space: 50.7 MB (40.23%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -9,23 +9,34 @@ class Solution {
         }
         // sort hashmap using stream 
         
-        List<Integer> mapSortedByValueDesc  = 
+  /*      List<Integer> mapSortedByValueDesc  = 
             map.entrySet().stream()
         .sorted((c1, c2) -> c2.getValue().compareTo(c1.getValue()))
         .limit(k)
         .map(i -> i.getKey())
         .collect(Collectors.toList());
         
+    */    
+        // new appproch start
         
-
-
-        int[] ans = new int[mapSortedByValueDesc.size()];
+Queue<Integer> pq = new PriorityQueue<>((a,b) ->(map.get(a)-map.get(b))); 
+        // PQ initialized to sort based on frequency at insertion.
         
-        for(int j=0; j < mapSortedByValueDesc.size() ; j++)
-            {
-                ans[j] = mapSortedByValueDesc.get(j);
-            }
+        for(int i : map.keySet()){   
+            // keyset to get all nums from map keys
+            pq.add(i);
+            if(pq.size()>k) pq.poll(); 
+       // checking if we reached Kth position, then delete the first value(value with least frequency yet encountered)
+        }
 
-        return ans ;  
+
+//         int[] ans = new int[pq.size()];
+        
+//         for(int j=0; j < pq.size() ; j++)
+//             {
+//                 ans[j] = pq.poll();
+//             }
+
+        return pq.stream().mapToInt(Integer::intValue).toArray();  
     }
 }
