78ef36e5b32e5e9e36d2000b52452e9b4d3380d7
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-10-25 10:28:56 +0530

    Time: 6 ms (6.41%), Space: 42 MB (56.33%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -1,25 +1,24 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         
-         int arr1=word1.length;
-    int arr2=word2.length;
-    StringBuilder sst1=new StringBuilder();
-    StringBuilder sst2=new StringBuilder();
-    
-    for(int i=0;i<arr1;i++){
-        sst1.append(word1[i]);
+        StringBuilder str1 = new StringBuilder();
+        
+        StringBuilder str2 = new StringBuilder();
+        
+        for(String word : word1)
+        {
+            str1.append(word);
+        }
+         for(String words : word2)
+        {
+            str2.append(words);
+             
+             
+        }
+        System.out.println(str1);
+        System.out.println(str2);
+        
+        return str1.toString().equals(str2.toString());
+        
     }
-    for(int j=0;j<arr2;j++){
-        sst2.append(word2[j]);
-    }
-    
-    String name1=sst1.toString();
-    String name2=sst2.toString();
-    
-    
-    
-    return name1.equals(name2);
-    
-}
-   
 }
