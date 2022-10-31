79c4ac360f3311e7764b84394294b2f2bb80718c
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-10-31 09:18:39 +0530

    Time: 43 ms (5.79%), Space: 48.4 MB (76.73%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,19 +1,41 @@
 class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
+        
+        // here we are going to use prefix Sum technique 
+        
+        
+        int n = nums.length ;
+        
         Arrays.sort(nums);
-        for(int i=1;i<nums.length;i++)
+       // int[] prefsum = new prefsum[n];
+        
+        for(int i=1; i < n ; i++)
         {
-            nums[i]=nums[i]+nums[i-1];
-        }
-        int ans[]=new int[queries.length];
-        for(int i=0;i<queries.length;i++)
+            
+           nums[i]= nums[i] + nums[i-1] ;
+            
+         }
+        
+        int index = 0; 
+     int count=0;
+        int ans[] = new int[queries.length] ;
+        for( int j =0 ; j < queries.length ; j++)
         {
-            for(int j=0;j<nums.length;j++)
+            
+            for(int i =0 ; i < n ; i++)
             {
-                if(nums[j]<=queries[i])ans[i]+=1;
-                else break;
+                //count = 0 ;
+                if(nums[i] <= queries[j] )
+                {
+                    ans[j] += 1 ;
+                }
+                
+                //ans[j]=count ;
             }
+            
+            
         }
-        return ans;
+        
+        return ans ; 
     }
 }
