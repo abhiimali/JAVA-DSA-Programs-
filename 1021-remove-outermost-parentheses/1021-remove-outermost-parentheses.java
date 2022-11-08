f3dae97d83f18bb64c000117687df4d8bf1e5411
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-11-08 18:53:44 +0530

    Time: 44 ms (16.12%), Space: 66.9 MB (17.34%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,20 +1,29 @@
 class Solution {
     public String removeOuterParentheses(String s) {
         
-        String sb="";
-int level = 0;
-for(int i = 0;i<s.length();i++){
-if(s.charAt(i) == '('){
-level++;
-}
-if(level > 1){
-sb+=s.charAt(i);
-}
-if(s.charAt(i) == ')'){
-level--;
-}
-}
-return sb;
+        int n = s.length();
+        String str ="" ;
+        int level = 0 ;
+        for(int i=0 ;  i<n ; i++)
+        {
+          //  char ch = s.charAt(i);
+            
+            if( s.charAt(i) == '(')
+            {
+                level++;
+            }
+            if(level > 1)
+            {
+                str += s.charAt(i) ;
+            }
+            if(s.charAt(i) == ')')
+            {
+                
+                level-- ;
+            }
+            
+        }
+        return str ; 
         
     }
 }
