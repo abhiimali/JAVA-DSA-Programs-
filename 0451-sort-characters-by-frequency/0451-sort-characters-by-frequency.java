6212cfc0283529d518d19ce8a136f415c68eeca6
Abhijit Mali <55556198+abhiimali@users.noreply.github.com>
2022-12-04 13:51:27 +0530

    Time: 39 ms (52.72%), Space: 49.1 MB (57.04%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -2,36 +2,34 @@ class Solution {
     public String frequencySort(String s) {
         
         HashMap<Character,Integer> map = new HashMap<>();
-PriorityQueue<Character> pq = new PriorityQueue<>( (a,b) -> map.get(b) - map.get(a));
-
+        
+PriorityQueue<Character> pq= new PriorityQueue<>((a,b) -> map.get(b)-map.get(a));
         for(char c : s.toCharArray())
         {
             map.put(c,map.getOrDefault(c,0)+1);
         }
-        // now offer pq 
         
         for(char c : map.keySet())
+            
         {
             pq.offer(c);
+            
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!pq.isEmpty())
+        {
+            int n = map.get(pq.peek());
+            char ck = pq.poll();
+            while(n>0)
+            {
+                sb.append(ck);
+                n-- ;
+            }
+            
         }
         
-        StringBuilder sb = new StringBuilder();
-        
-         while(!pq.isEmpty())
-         {
-             char ch = pq.poll();
-             for(int j=0; j< map.get(ch);j++) sb.append(ch);
-             
-         }
-        
         return sb.toString();
+        
+        
     }
 }
-
-
-
-// Approach  
-
-// 1. Put All character in hashmap with frequency 
-// 2. make a priorityQueue With Custome Comparator and sort char on basis of freq 
-// 3. pq -> sb 
